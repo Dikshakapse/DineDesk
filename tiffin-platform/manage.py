f53ba6b0b0255009b52bd43239b11913d51e15d3
Sakshi Mote <sakshimote20@gmail.com>
2025-04-06 15:57:23 +0530

    ..

--- tiffin-platform/manage.py
+++ tiffin-platform/manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+# ✅ Add backend to sys.path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
+
+# ✅ Correct settings module path (because tiffin_backend is inside backend folder)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tiffin_backend.settings')
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tiffin_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +20,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
