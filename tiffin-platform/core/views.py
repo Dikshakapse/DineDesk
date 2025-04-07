7ce365e6c1381d7340d82277c10320fc3f1993b4
vaishnaviwalunj <vaishnaviwalunj5325@gmail.com>
2025-04-07 20:20:04 +0530

    ..
    ///

--- tiffin-platform/core/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from .models import Tiffin
-
-def home(request):
-    tiffins = Tiffin.objects.all()
-    return render(request, 'core/home.html', {'tiffins': tiffins})
